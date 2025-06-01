af928becc87bdf3207982c6262d02d1d51f4429f
chlwoomin <chlwoomin@naver.com>
2025-06-01 22:10:28 +0900

    sz 핸들링 수정

--- xv6/exec.c
+++ xv6/exec.c
@@ -66,6 +66,7 @@ exec(char *path, char **argv)
   // 2*PGSIZE로 하면 페이지의 끝 주소가 커널 베이스가 되기 때문에 한 단계 더 내린다.
   // Pagefault가 발생했을 때 스택의 바로 아래인지를 판단하기 위해 gaurd page도 할당한다.
   cprintf("[exec] sz %x curproc pid %d\n",sz,curproc->pid);
+  uint sz0 = sz;
   sz = PGROUNDDOWN(KERNBASE - 2*PGSIZE);
   // 커널 베이스에서 PGSIZE만큼 할당
   if((sz = allocuvm(pgdir, sz, sz + PGSIZE)) == 0)
@@ -75,7 +76,7 @@ exec(char *path, char **argv)
   // 0xb98은 text, data영역의 윗 부분
   // sz는 사용 중인 유저 공간을 나타내주는데 스택을 kernbase로 옮겨서
   // 스택 외의 코드까지만 sz로 변경
-  sz = PGROUNDUP(0xb98) /*+ 2*PGSIZE*/;
+  sz = PGROUNDUP(sz0);
   cprintf("[exec] sz %x curproc pid %d\n",sz,curproc->pid);
 
 
